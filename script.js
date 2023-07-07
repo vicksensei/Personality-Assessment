ed23f7a597bfe2606b2d9c2fdd5e28e16204c436
Victor Payano <vick.sensei@gmail.com>
2023-07-07 00:42:58 -0500

    Added clear buttones pretiefied css

--- script.js
+++ script.js
@@ -14,17 +14,16 @@ function clickPoint(valName, valAmount) {
   const poolElement = document.getElementById("pool");
   let stat = stats[valName];
   const statElement = document.getElementById(`${valName}_span`);
+  const pointCost = stat - valAmount;
   if (stat === valAmount) {
     return;
   }
-  if (stat - valAmount > pool) {
+  if (Math.abs(stat - valAmount) > pool) {
     return;
   }
   pool += stat - valAmount;
   stats[valName] = valAmount;
   statElement.innerHTML = `${valName} : ${stats[valName]} `;
-  console.log("stat-valAmount :>> ", stat - valAmount);
-  console.log("pool :>> ", pool);
   poolElement.innerHTML = pool;
 
   for (let i = 1; i <= 3; i++) {
@@ -42,10 +41,40 @@ function clickPoint(valName, valAmount) {
     }
   }
 }
-window.addEventListener("load", (event) => {
+function clearAll() {
+  pool = 10;
+  Object.keys(stats).map((s) => {
+    stats[s] = 0;
+  });
+  initSection();
+}
+
+function clearStat(stat) {
+  pool += stats[stat];
+  stats[stat] = 0;
+  const spanID = `${stat}_span`;
+  const spanElement = document.getElementById(spanID);
+  const poolElement = document.getElementById("pool");
+  //   console.log("Clearing:", stat);
+  for (let i = 1; i <= 3; i++) {
+    const item = document.getElementById(`${stat}_p${i}`);
+    if (item.classList.contains("selected")) {
+      item.classList.remove("selected");
+      item.classList.add("gray");
+    }
+  }
+  spanElement.innerHTML = ` ${stat} : ${stats[stat]} `;
+  poolElement.innerHTML = 10;
+}
+
+function initSection() {
+  content.innerHTML =
+    `<h1>Personality Self Assessment</h1>` +
+    `<div> Points: <div id="pool"> 10 </div></div> `;
   Object.keys(stats).map((s) => {
     content.innerHTML +=
       `<div class='stat' id='${s}'> ` +
+      `<div class='clear' onClick='clearStat("${s}")'>X</div>` +
       `<span id='${s}_span'>${s} : ${stats[s]} </span>` +
       `<div class='points'>` +
       `<div class='point gray' id='${s}_p1' onClick='clickPoint("${s}", 1)'></div>` +
@@ -54,5 +83,8 @@ window.addEventListener("load", (event) => {
       "</div></div>";
   });
 
-  //   for
+  content.innerHTML += `<div class="button" onClick='clearAll()'>Clear</div>`;
+}
+window.addEventListener("load", (event) => {
+  initSection();
 });
